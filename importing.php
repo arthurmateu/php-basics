0bc1f3419b53af351bebfa8243d33ff5f57b183f
admouse <arthurmateu@gmail.com>
2024-11-06 01:16:21 +0100

    files

---
+++ importing.php
@@ -0,0 +1,11 @@
+<?php
+
+// you can use `include` or `require`, depending on your needs
+// can also be `{x}_once`. self-explanatory.
+include 'functions.php';
+
+// Note how it will run everything in `functions.php`.
+
+echo addition(2, 2);
+
+?>
